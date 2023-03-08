eda601e0ca39769ff414e0ddcad25809c64d6f07
esoastor <hazehelt@gmail.com>
2023-03-08 22:18:58 +0300

    fix

--- src/Errors/InvalidLength.php
+++ src/Errors/InvalidLength.php
@@ -5,6 +5,6 @@ class InvalidLength extends \Exception
 {
     public function __construct(int $maximumLength)
     {
-        $this->message("Field length is too big. Maximum length is $maximumLength\n");
+        $this->message = "Field length is too big. Maximum length is $maximumLength\n";
     }
 }
